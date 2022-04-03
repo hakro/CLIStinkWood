081d8f4a67dbe96b85c4dc90b36cd27d6828d0fb
Hakim <hakim.rouatbi@gmail.com>
2022-04-03 14:16:36 +0200

    add enemies and stuff

--- Main.gd
+++ Main.gd
@@ -1,11 +1,13 @@
-extends Node2D
+extends MarginContainer
 
+# Player stats
 var firewall := 100
 var balance := 50
 
-onready var firewall_label := $MarginContainer/VBoxContainer/HBoxContainer/HSplitContainer/LeftSplit/MarginContainer/VBoxContainer/Stats/FirewallBar
-onready var balance_label := $MarginContainer/VBoxContainer/HBoxContainer/HSplitContainer/LeftSplit/MarginContainer/VBoxContainer/Stats/BalanceValue
+onready var firewall_label := $VBoxContainer/HBoxContainer/HSplitContainer/LeftSplit/MarginContainer/VBoxContainer/Stats/FirewallBar
+onready var balance_label := $VBoxContainer/HBoxContainer/HSplitContainer/LeftSplit/MarginContainer/VBoxContainer/Stats/BalanceValue
 
 func _ready() -> void:
 	firewall_label.value = 100
 	balance_label.text = "$ " + String(balance)
+
